4af9fdec426ae1b1285141dbaad50d33721f7906
LiHuaGO <137274378+LiHuaGO@users.noreply.github.com>
2026-01-05 09:11:05 +0800

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,15 +1,13 @@
 from ultralytics import YOLO,RTDETR
 import torch
-# torch.use_deterministic_algorithms(False)  # 关闭确定性计算
-# torch.backends.cudnn.deterministic = False  # 关闭 cuDNN 确定性
 if __name__ == '__main__':
 
-    yaml = r"rtdetr-n.yaml"
-    data = r"/data/lihua/超声/中文期刊_部分数据/YOLO13/data80%_11.19/dataset.yaml"
+    yaml = r"yolov13n.yaml"
+    data = r"dataset.yaml"
     project = "腹部"
-    name = "rtdetr-n"
+    name = "yolov13n"
     path = project + "/" + name
-    model = RTDETR(yaml)#YOLO(yaml, task="detect")
+    model = YOLO(yaml, task="detect")
     model.train(data=data, project=project, name=name, imgsz=640, batch=128,patience=False,
                     epochs=300, device=[0,1,2,3,4,5,6,7], exist_ok=True,cos_lr=True,pretrained=True)
 
